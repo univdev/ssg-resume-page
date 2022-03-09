06cbcbc77b5f23238bb41119b381d055b15816b4
박찬영 <pcydeveloper@gmail.com>
2022-03-10 00:18:39 +0900

    refactor: 프로젝트 템플릿 페이지 제거

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,11 @@
-<script lang="ts">
-	import Nav from '../components/Nav.svelte';
+<script>
 
-	export let segment: string;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+
 </style>
 
-<Nav {segment}/>
-
-<main>
+<div class="layout">
 	<slot></slot>
-</main>
+</div>
