7f1ca705698e430b8fe93c9a738d342a0c97b606
박찬영 <pcydeveloper@gmail.com>
2022-03-17 23:58:43 +0900

    chore: firebase 세팅

--- functions/src/hooks/storage.js
+++ functions/src/hooks/storage.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const firestore = require('@google-cloud/firestore');
+const db = new firestore();
 
 exports.finalized = functions.storage.object().onFinalize(async (response) => {
   console.log(response);
+  // await db.collection('storage').docs().set(response);
+  // return response;
 });
 exports.deleted = functions.storage.object().onDelete(async (response) => {
-  console.log(response);
+  
 });
