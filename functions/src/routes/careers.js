9d64bac46e40b0f2783df42c6f0c3106b7dcc90e
박찬영 <pcydeveloper@gmail.com>
2022-03-19 23:03:56 +0900

    feat: careers API 추가

--- functions/src/routes/careers.js
+++ functions/src/routes/careers.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { admin } = require('../helpers/admin');
 
-router.get('/', (req, res) => {
-  return res.json({
-    result: [
-      { data: 1 },
-    ],
-  });
+router.get('/', async (req, res) => {
+  try {
+    const snapshot = await admin.firestore().collection('careers').get();
+    const items = [...snapshot.docs].map((doc) => doc.data());
+    return res.json({
+      result: [...items],
+    });
+  } catch (e) {
+    return res.status(500).json(e);
+  } 
 });
 
 module.exports = router;
