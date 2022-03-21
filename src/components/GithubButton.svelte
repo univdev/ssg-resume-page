a0c4b42da5306a066add9cb0a2a5327fe4cb6462
박찬영 <pcydeveloper@gmail.com>
2022-03-22 02:02:32 +0900

    feat: Github, Notion 링크 추가

---
+++ src/components/GithubButton.svelte
@@ -0,0 +1,20 @@
+<style lang="less">
+  .github-button {
+    background-color: var(--bs-dark);
+    color: white;
+    border-radius: 20px;
+    padding: 16px 24px;
+    font-size: 18px;
+    font-weight: bold;
+    .icon {
+      margin-right: 8px;
+    }
+  }
+</style>
+
+<button class="github-button">
+  <i class="icon fab fa-github"></i>
+  <span>Github</span>
+</button>
+
+<script lang="ts"></script>
--- src/routes/.components/MainSection.svelte
+++ src/routes/.components/MainSection.svelte
@@ -43,6 +43,18 @@
         }
       }
     }
+    .references {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 48px;
+      .references__item {
+        display: block;
+        & + .references__item {
+          margin-left: 16px;
+        }
+      }
+    }
   }
 </style>
 
@@ -66,6 +78,20 @@
         풍부한 프로젝트 리딩 경험을 겸비한 프로그래머 박찬영입니다<br>
         논리에 기반한 사고를 바탕으로 보다 깔끔한 코드와 알고리즘을 구현하려고 노력합니다.
       </p>
+      <nav class="references">
+        <a
+          target="_blank"
+          href="https://github.com/univdev"
+          class="github-link references__item">
+          <GithubButton></GithubButton>
+        </a>
+        <a
+          target="_blank"
+          href="https://univdev.notion.site"
+          class="notion-link references__item">
+          <NotionButton></NotionButton>
+        </a>
+      </nav>
     </div>
   </section>
 </template>
@@ -73,6 +99,8 @@
 <script lang="ts">
   import MainBackgroundImage from 'images/main-background.jpg';
   import TypingAnimation from '../../components/TypingAnimation.svelte';
+  import GithubButton from '../../components/GithubButton.svelte';
+  import NotionButton from '../../components/NotionButton.svelte';
   
   export let descriptions: Array<string>;
 </script>
